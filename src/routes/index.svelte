862f0635a4885f9a01b19a5e14479acaf87d48c2
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-04-05 14:05:54 +0100

    Override theme colour in example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,23 @@
 <script>
-	import Donate from '$lib/SvelteNanoDonate.svelte';
-
+  import Donate from '$lib/SvelteNanoDonate.svelte'
 </script>
 
 <main>
-	<Donate
-		amount=5
-		currency=usd
-		address=nano_1hfmyyjtm4muiudw1m8dz54jry13jx8xykp9kz1mx3uqe4dtsb3yrdkjgy6g
-		theme={{'colour': '#3399ff'}}
-	/>
+  <Donate
+    amount=5
+    currency=usd
+    address=nano_1hfmyyjtm4muiudw1m8dz54jry13jx8xykp9kz1mx3uqe4dtsb3yrdkjgy6g
+    theme={{colour: '#336699'}}
+  />
 </main>
+
+<style>
+  :root {
+    background-color: white;
+  }
+  @media (prefers-color-scheme: dark) {
+    :root {
+      background-color: black;
+    }
+  }
+</style>
